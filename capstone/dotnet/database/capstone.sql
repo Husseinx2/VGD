afa5120981aff2f8a34d26092a33973b556fa194
hussein hussein <husseinwkc@gmail.com>
2023-08-04 16:49:57 -0400

    created and populated Tables for platforms/companies/genres

--- capstone/dotnet/database/capstone.sql
+++ capstone/dotnet/database/capstone.sql
@@ -87,6 +87,7 @@ CREATE TABLE game_genre (
 
 
 --populate default data
+--game
 INSERT INTO game (title, description, esrb_rating, release_date) VALUES ('Super Mario Bros','Super Mario Bros. is a platform game. In the game, Mario must race through the Mushroom Kingdom and save Princess Toadstool (later Princess Peach) from Bowser. Mario jumps, runs, and walks across each level. The worlds are full of enemies and platforms, and open holes.','E','09/13/1985');
 INSERT INTO game (title, description, esrb_rating, release_date) VALUES ('Grand Theft Auto V','Grand Theft Auto V is an action-adventure game played from either a third-person or first-person perspective. Players complete missions—linear scenarios with set objectives—to progress through the story. Outside of the missions, players may freely roam the open world.','M','09/17/2013');
 INSERT INTO game (title, description, esrb_rating, release_date) VALUES ('Prey',' In Prey, you awaken aboard Talos I, a space station orbiting the moon in the year 2032. You are the key subject of an experiment meant to alter humanity forever – but things have gone terribly wrong. The space station has been overrun by hostile aliens and you are now being hunted.','M','05/15/2017');
@@ -94,6 +95,79 @@ INSERT INTO game (title, description, esrb_rating, release_date) VALUES ('Prey',
 
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+--genre 
+INSERT INTO genre (genre_name) VALUES ('Platformer');
+INSERT INTO genre (genre_name) VALUES ('Third-Person Shooter');
+INSERT INTO genre (genre_name) VALUES ('First-Person Shooter');
+INSERT INTO genre (genre_name) VALUES ('Action');
+INSERT INTO genre (genre_name) VALUES ('Adventure');
+INSERT INTO genre (genre_name) VALUES ('Puzzle');
+INSERT INTO genre (genre_name) VALUES ('Open-World');
+INSERT INTO genre (genre_name) VALUES ('Horror');
+INSERT INTO genre (genre_name) VALUES ('Sports');
+INSERT INTO genre (genre_name) VALUES ('Role-Playing');
+INSERT INTO genre (genre_name) VALUES ('Fighting');
 
+INSERT INTO game_genre (game_id,genre_id) VALUES (1,1);
+INSERT INTO game_genre (game_id,genre_id) VALUES (2,2);
+INSERT INTO game_genre (game_id,genre_id) VALUES (2,3);
+INSERT INTO game_genre (game_id,genre_id) VALUES (2,4);
+INSERT INTO game_genre (game_id,genre_id) VALUES (2,5);
+INSERT INTO game_genre (game_id,genre_id) VALUES (2,6);
+INSERT INTO game_genre (game_id,genre_id) VALUES (2,7);
+INSERT INTO game_genre (game_id,genre_id) VALUES (3,3);
+INSERT INTO game_genre (game_id,genre_id) VALUES (3,5);
+
+
+--company
+INSERT INTO company (company_name) VALUES ('Nintendo');
+INSERT INTO company (company_name) VALUES ('Rockstar Games')
+INSERT INTO company (company_name) VALUES ('Arkane Studios')
+INSERT INTO company (company_name) VALUES ('2K')
+INSERT INTO company (company_name) VALUES ('Bungie')
+INSERT INTO company (company_name) VALUES ('Activision')
+INSERT INTO company (company_name) VALUES ('Arc System Works')
+INSERT INTO company (company_name) VALUES ('Artoon')
+INSERT INTO company (company_name) VALUES ('Bethesda')
+INSERT INTO company (company_name) VALUES ('BioWare')
+INSERT INTO company (company_name) VALUES ('Capcom')
+INSERT INTO company (company_name) VALUES ('Epic Games')
+INSERT INTO company (company_name) VALUES ('Ubisoft')
+INSERT INTO company (company_name) VALUES ('Sega')
+INSERT INTO company (company_name) VALUES ('Microsoft')
+INSERT INTO company (company_name) VALUES ('Square Enix')
+
+INSERT INTO game_developer (game_id,developer_id) VALUES (1,1);
+INSERT INTO game_developer (game_id,developer_id) VALUES (2,2);
+INSERT INTO game_developer (game_id,developer_id) VALUES (3,3);
+
+INSERT INTO game_publisher (game_id,publisher_id) VALUES (1,1);
+INSERT INTO game_publisher (game_id,publisher_id) VALUES (2,2);
+INSERT INTO game_publisher (game_id,publisher_id) VALUES (3,9);
+
+--Platform
+
+INSERT INTO platform (platform_name) VALUES ('NES');
+INSERT INTO platform (platform_name) VALUES ('Xbox 360');
+INSERT INTO platform (platform_name) VALUES ('Xbox one');
+INSERT INTO platform (platform_name) VALUES ('PlayStation 3');
+INSERT INTO platform (platform_name) VALUES ('PlayStation 4');
+INSERT INTO platform (platform_name) VALUES ('PlayStation 5');
+INSERT INTO platform (platform_name) VALUES ('PC');
+INSERT INTO platform (platform_name) VALUES ('Nintendo 64');
+INSERT INTO platform (platform_name) VALUES ('GameCube');
+INSERT INTO platform (platform_name) VALUES ('Xbox X/S');
+
+
+INSERT INTO game_platform (game_id,platform_id) VALUES (1,1);
+INSERT INTO game_platform (game_id,platform_id) VALUES (2,3);
+INSERT INTO game_platform (game_id,platform_id) VALUES (2,4);
+INSERT INTO game_platform (game_id,platform_id) VALUES (2,5);
+INSERT INTO game_platform (game_id,platform_id) VALUES (2,6);
+INSERT INTO game_platform (game_id,platform_id) VALUES (2,7);
+INSERT INTO game_platform (game_id,platform_id) VALUES (2,10);
+INSERT INTO game_platform (game_id,platform_id) VALUES (3,5);
+INSERT INTO game_platform (game_id,platform_id) VALUES (3,3);
+INSERT INTO game_platform (game_id,platform_id) VALUES (3,7);
 
 GO
