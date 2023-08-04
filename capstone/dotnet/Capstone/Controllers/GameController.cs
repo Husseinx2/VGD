6eea2b40f116a22f9d5591e607764b332aa49a3e
Kevin Lie <kevinyilie@gmail.com>
2023-08-04 18:07:43 -0400

    Add GameSqlDao dependecy injection

--- capstone/dotnet/Capstone/Controllers/GameController.cs
+++ capstone/dotnet/Capstone/Controllers/GameController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Capstone.DAO.Interfaces;
+using Capstone.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Capstone.Controllers
 {
@@ -7,5 +9,50 @@ namespace Capstone.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
+        public IGameDao gameDao;
+
+        public GameController(IGameDao gameDao)
+        {
+            this.gameDao = gameDao;
+        }
+
+        [HttpGet()]
+        public ActionResult<List<Game>> GetGames()
+        {
+            return Ok(gameDao.ListGames());
+        }
+
+        [HttpPost()]
+        public ActionResult<Game> AddGame(Game game)
+        {
+            Game newGame = gameDao.AddGame(game);
+
+            if (newGame == null || newGame.Id == 0)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(newGame);
+            }
+        }
+
+        [HttpPut("{gameId}")]
+        public ActionResult<Game> UpdateGame(Game game)
+        {
+            Game newGame = gameDao.UpdateGame(game);
+
+
+            if (newGame == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(newGame);
+            }
+
+        }
+
     }
 }
