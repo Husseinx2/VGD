cf727e963ebdf89eb3f221ce329824036b6af9ea
hussein hussein <husseinwkc@gmail.com>
2023-08-04 17:35:37 -0400

    show game

---
+++ capstone/vue/src/components/GameCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1> 
+       {{item.id}} || {{item.title}} || {{item.description}} || {{item.release_date}}
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['item']
+}
+</script>
+
+<style>
+
+</style>
--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -2,11 +2,65 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <input placeholder="Search Games"  type="text" v-model="search.title" />
+    <select v-model="select"> 
+      <option v-for="game in games" v-bind:key="game.id" >
+       {{game.title}}
+    </option>
+    </select>
+    <game-card-vue
+      v-for="game in games"
+      v-bind:key="game.id"
+      v-bind:item="game"
+    />
   </div>
 </template>
 
 <script>
+import GameCardVue from "../components/GameCard.vue";
 export default {
-  name: "home"
+  name: "home",
+  data() {
+    return {
+      search:"",
+      select: {
+        id: "0",
+        title: "",
+        release_date: "",
+        description: "",
+      },
+      games: [
+        
+         {
+          id: 1,
+          title: "Mario Bros",
+          release_date: "09/13/1983",
+          description: "a game",
+        },
+        {
+          id: 1,
+          title: "Red Dead Redemption",
+          release_date: "09/13/2019",
+          description: "a cowboy game",
+        },
+        {
+          id: 1,
+          title: "Grand Theft Auto V",
+          release_date: "09/17/2013",
+          description: "a violent game",
+        },
+      ],
+    };
+  },
+  components: { GameCardVue },
+  computed: {
+     filteredList(){
+       return this.games.filter((game) => {
+         return (
+            game.title.toLowerCase().includes(this.search)
+         )
+       })
+     }
+  }
 };
 </script>
