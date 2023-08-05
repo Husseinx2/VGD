a4d118a295087da3af9402c941ecced2d476eb2f
Kevin Lie <kevinyilie@gmail.com>
2023-08-05 16:16:18 -0400

    Fix bugs in UpdateGame

--- capstone/dotnet/Capstone/DAO/GameSqlDao.cs
+++ capstone/dotnet/Capstone/DAO/GameSqlDao.cs
@@ -11,14 +11,15 @@ namespace Capstone.DAO
         private string connectionString = "";
 
         private string sqlListGames = "SELECT game_id, title, description, esrb_rating, release_date FROM game";
-        private string sqlGetGame = "SELECT game_id, title, description, esrb_rating, release_date from game WHERE game_id = @id;";
+        private string sqlGetGame = "SELECT game_id, title, description, esrb_rating, release_date from game WHERE game_id = @game_id;";
         private string sqlAddGame = "INSERT INTO game (title, description, esrb_rating, release_date) " +
             "OUTPUT INSERTED.game_id " +
             "VALUES (@title, @description, @esrb_rating, @release_date);";
         private string sqlUpdateGame = "UPDATE game SET title=@title, description=@description, esrb_rating=@esrb_rating, " +
             "release_date=@release_date " +
-            "WHERE id = @id;";
-        private string sqlDeleteGame = "DELETE FROM game WHERE game_id=@id;";
+            "WHERE game_id = @game_id;";
+        // TODO: Update sqlDeleteGame to delete game with foreign key contraints
+        private string sqlDeleteGame = "DELETE FROM game WHERE game_id=@game_id;";
 
         public GameSqlDao(string connectionString)
         {
@@ -32,22 +33,19 @@ namespace Capstone.DAO
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-
                 using (SqlCommand cmd = new SqlCommand(sqlListGames, conn))
                 {
-
                     using (SqlDataReader reader = cmd.ExecuteReader())
                     {
-
                         while (reader.Read())
                         {
-                            Game game = new Game();
-                            game = MapRowToGame(reader);
+                            Game game = MapRowToGame(reader);
                             games.Add(game);
                         }
                     }
                 }
             }
+
             return games;
         }
 
@@ -58,11 +56,9 @@ namespace Capstone.DAO
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-
                 using (SqlCommand cmd = new SqlCommand(sqlGetGame, conn))
                 {
-                    cmd.Parameters.AddWithValue("@id", gameId);
-
+                    cmd.Parameters.AddWithValue("@game_id", gameId);
                     using (SqlDataReader reader = cmd.ExecuteReader())
                     {
                         if (reader.Read())
@@ -72,28 +68,29 @@ namespace Capstone.DAO
                     }
                 }
             }
+
             return game;
         }
 
-        public Game AddGame(Game newGame)
+        public Game AddGame(Game game)
         {
-            newGame.Id = 0;
+            game.Id = 0;
 
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
                 using (SqlCommand cmd = new SqlCommand(sqlAddGame, conn))
                 {
-                    cmd.Parameters.AddWithValue("@title", newGame.Title);
-                    cmd.Parameters.AddWithValue("@description", newGame.Description);
-                    cmd.Parameters.AddWithValue("@esrb_rating", newGame.ESRBRating);
-                    cmd.Parameters.AddWithValue("@release_date", newGame.ReleaseDate);
+                    cmd.Parameters.AddWithValue("@title", game.Title);
+                    cmd.Parameters.AddWithValue("@description", game.Description);
+                    cmd.Parameters.AddWithValue("@esrb_rating", game.ESRBRating);
+                    cmd.Parameters.AddWithValue("@release_date", game.ReleaseDate);
 
-                    newGame.Id = (int)cmd.ExecuteScalar();
+                    game.Id = (int)cmd.ExecuteScalar();
                 }
             }
 
-            return GetGame(newGame.Id);
+            return GetGame(game.Id);
         }
 
         public Game UpdateGame(Game game)
@@ -101,31 +98,29 @@ namespace Capstone.DAO
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-
                 using (SqlCommand cmd = new SqlCommand(sqlUpdateGame, conn))
                 {
+                    cmd.Parameters.AddWithValue("@game_id", game.Id);
                     cmd.Parameters.AddWithValue("@title", game.Title);
                     cmd.Parameters.AddWithValue("@description", game.Description);
-                    cmd.Parameters.AddWithValue("@type", game.ESRBRating);
-                    cmd.Parameters.AddWithValue("@esrb_rating", game.ReleaseDate);
+                    cmd.Parameters.AddWithValue("@esrb_rating", game.ESRBRating);
+                    cmd.Parameters.AddWithValue("@release_date", game.ReleaseDate);
 
                     int count = cmd.ExecuteNonQuery();
 
-                    return  count == 1 ? game : null;
+                    return count == 1 ? game : null;
                 }
             }
         }
-
 
         public bool DeleteGame(int gameId)
         {
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-
                 using (SqlCommand cmd = new SqlCommand(sqlDeleteGame, conn))
                 {
-                    cmd.Parameters.AddWithValue("@id", gameId);
+                    cmd.Parameters.AddWithValue("@game_id", gameId);
 
                     int count = cmd.ExecuteNonQuery();
                     return count == 1;
@@ -136,13 +131,14 @@ namespace Capstone.DAO
         private Game MapRowToGame(SqlDataReader reader)
         {
             Game game = new Game();
+
             game.Id = Convert.ToInt32(reader["game_id"]);
             game.Title = Convert.ToString(reader["title"]);
             game.Description = Convert.ToString(reader["description"]);
             game.ESRBRating = Convert.ToString(reader["esrb_rating"]);
             game.ReleaseDate = Convert.ToDateTime(reader["release_date"]);
+
             return game;
-        }
-        
+        }    
     }
 }
