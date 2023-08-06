e25d9844401c3abca6d4a97909177d100225fb6f
Jimmie Nakadaira <moldychip@hotmail.com>
2023-08-06 18:17:32 -0400

    navbar editing

--- capstone/vue/src/components/NavBar.vue
+++ capstone/vue/src/components/NavBar.vue
@@ -5,7 +5,8 @@
       <p>VGD</p>
         </td>
         <td>
-     <input placeholder="Search"  type="text" v-model="search" :for="game in filteredList"
+     <input placeholder="Search"  type="text" v-model="search" 
+        v-for="game in filteredList"
         v-bind:key="game.id"
         v-bind:item="game"/>
         </td>
@@ -17,9 +18,49 @@
 </template>
 
 <script>
-
+import gameService from "../services/GameService";
 export default {
+  data() {
+    return {
+      newGame: {},
+      search: "",
 
+      games: [],
+    };
+  },
+    computed: {
+    filteredList() {
+      return this.games.filter((game) => {
+        return game.title.toLowerCase().includes(this.search);
+      });
+    },
+  },
+    methods: {
+        loadGames() {
+      gameService
+        .list()
+        .then((response) => {
+          console.log("Reached created in Home.vue");
+          console.log(response);
+          this.games = response.data;
+        })
+        .catch((error) => {
+          if (error.response) {
+            // error.response exists
+            // Request was made, but response has error status (4xx or 5xx)
+            console.log("Error loading games: ", error.response.status);
+          } else if (error.request) {
+            // There is no error.response, but error.request exists
+            // Request was made, but no response was received
+            console.log("Error loading games: unable to communicate to server");
+          } else {
+            // Neither error.response and error.request exist
+            // Request was *not* made
+            console.log("Error loading games: make request");
+          }
+        });
+    },
+    }
 }
 </script>
 
