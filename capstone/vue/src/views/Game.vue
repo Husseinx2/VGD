68d92507e12e5c36073fb350bcdddcaaf067ce7b
Kevin Lie <kevinyilie@gmail.com>
2023-08-06 17:17:51 -0400

    Add catch statement in gameGame

--- capstone/vue/src/views/Game.vue
+++ capstone/vue/src/views/Game.vue
@@ -18,14 +18,28 @@ export default {
   },
   created() {
     this.id = this.$route.params.id;
-     gameService.getGame(this.id)
-     .then((response) => {
-       this.game = response.data; 
+    gameService
+      .getGame(this.id)
+      .then((response) => {
+        this.game = response.data;
       })
+      .catch((error) => {
+        if (error.response) {
+          // error.response exists
+          // Request was made, but response has error status (4xx or 5xx)
+          console.log("Error getting game: ", error.response.status);
+        } else if (error.request) {
+          // There is no error.response, but error.request exists
+          // Request was made, but no response was received
+          console.log("Error getting game: unable to communicate to server");
+        } else {
+          // Neither error.response and error.request exist
+          // Request was *not* made
+          console.log("Error getting game: make request");
+        }
+      });
   },
-         
-}
-
+};
 </script>
 
 <style>
