2e2f7381913b8f9e017360852b8847a54133fe6f
Kevin Lie <kevinyilie@gmail.com>
2023-08-06 18:15:07 -0400

    Redirect game page with invalid id to notFound

--- capstone/vue/src/views/Game.vue
+++ capstone/vue/src/views/Game.vue
@@ -22,6 +22,9 @@ export default {
       .getGame(this.id)
       .then((response) => {
         this.game = response.data;
+        if (!this.game) {
+          this.$router.push({name: 'notFound'});
+        }
       })
       .catch((error) => {
         if (error.response) {
@@ -38,6 +41,8 @@ export default {
           console.log("Error getting game: make request");
         }
       });
+
+ 
   },
 };
 </script>
