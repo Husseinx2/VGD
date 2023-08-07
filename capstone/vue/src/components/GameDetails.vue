627135ada9c302183e604636b2462ac2d4cf7d11
hussein hussein <husseinwkc@gmail.com>
2023-08-07 13:22:50 -0400

    details page grid layout

--- capstone/vue/src/components/GameDetails.vue
+++ capstone/vue/src/components/GameDetails.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="container">
-    <h1>{{ item.title }}</h1>
+    <h1 class="title">{{ item.title }}</h1>
     Description:
-    <p>{{ item.description }}</p>
+    <p class="description">{{ item.description }}</p>
 
-    <ul>
+    <ul class="platforms">
       Platforms:
       <li v-for="platform in item.platforms" v-bind:key="platform">
         {{ platform }}
       </li>
     </ul>
-    <ul>
+    <ul class="developers">
       Developers:
       <li v-for="developer in item.developers" v-bind:key="developer">
         {{ developer }}
       </li>
     </ul>
-    <ul>
+    <ul class="publishers">
       Publishers:
       <li v-for="publisher in item.publishers" v-bind:key="publisher">
         {{ publisher }}
       </li>
     </ul>
-    <ul>
+    <ul class="genres">
       Genres:
       <li v-for="genre in item.genres" v-bind:key="genre">
         {{ genre }}
@@ -42,20 +42,32 @@ export default {
   position: relative;
   top: 250px;
   display: grid;
-  grid-template: 1fr 1fr 1fr;
+  grid-template: 1fr 1fr ;
   grid-template-areas:
-    ". title ."
-    "description description description"
-    "genre . ."
-    "platforms . ."
-    "developers . ."
-    "publisher . ."
+    ". title "
+    "description"
+    "genre "
+    "platforms "
+    "developers "
+    "publishers "
 }
 .title {
   grid-area: "title";
 }
 .description {
-
+grid-area: "description";
+}
+.genre {
+  grid-area: genre;
+}
+.platforms {
+  grid-area:"platforms"
+}
+.developers{
+  grid-area: "developers";
+}
+.publishers { 
+  grid-area: "publishers";
 }
 P,
 h1,
