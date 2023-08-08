9f1845bac29b2ecc4ab895927198ed33b908b34e
Jimmie Nakadaira <moldychip@hotmail.com>
2023-08-08 16:22:06 -0400

    added the restrictions from addGame to EditGame

--- capstone/vue/src/components/EditGameForm.vue
+++ capstone/vue/src/components/EditGameForm.vue
@@ -1,144 +1,162 @@
 <template>
-  <div>
-    <b-form class="edit" v-on:click.prevent>
-      <label for="title">Title: </label>
+  <b-form @submit.stop.prevent="onSubmit">
+    <b-form-group class="titleInput">
+      <label>Title</label>
       <b-form-input
-        v-model="game.title"
-        :state="titleState"
-        name="title"
+        class="form__input"
+        id="titleInput"
+        v-model="$v.game.title.$model"
+        :state="validateState('title')"
         type="text"
+        placeholder="Game Title"
+        required
         aria-describedby="input-live-feedback"
-      />
+      ></b-form-input>
       <b-form-invalid-feedback id="input-live-feedback"
         >Enter at least 1 letter</b-form-invalid-feedback
       >
-      <br />
-      <label for="description">Description:</label>
-      <br />
+    </b-form-group>
+    <b-form-group
+      id="description"
+      label="Description"
+      label-for="descriptionInput"
+    >
       <b-form-textarea
-        name="description"
-        v-model="game.description"
-        :state="descriptionState"
+        id="descriptionInput"
+        v-model.trim="$v.game.description.$model"
+        :state="validateState('description')"
         aria-describedby="input-live-feedback2"
-      >
-      </b-form-textarea>
+        type="text"
+        placeholder="Game Description"
+        required
+      ></b-form-textarea>
       <b-form-invalid-feedback id="input-live-feedback2"
         >Enter at least 1 letter</b-form-invalid-feedback
       >
-      <br />
-      <label for="esrbRating">ESRB Rating:</label>
+    </b-form-group>
+    <!-- esrb rating -->
+    <b-form-group>
+      <label for="description">ESRB Rating:</label>
       <b-form-select
         :options="options"
         type="text"
         name="rating"
-        v-model="game.esrbRating"
-        :state="esrbRatingState"
+        v-model="$v.game.esrbRating.$model"
+        :state="validateState('esrbRating')"
         aria-describedby="input-live-feedback3"
+        required
       ></b-form-select>
       <b-form-invalid-feedback id="input-live-feedback3"
-        >Please select a valid input</b-form-invalid-feedback
+        >Enter a valid rating</b-form-invalid-feedback
       >
-      <br />
-      <br />
+    </b-form-group>
+    <!-- date -->
+    <b-form-group>
       <label for="date">Release Date:</label>
       <b-form-datepicker
-        v-model="game.releaseDate"
-        :state="releaseDateState"
+        v-model="$v.game.releaseDate.$model"
+        :state="validateState('releaseDate')"
+        aria-describedby="input-live-4"
         type="date"
         name="date"
-        aria-describedby="input-live-feedback4"
+        required
       />
-      <b-form-invalid-feedback id="input-live-feedback4"
-        >Please select a valid date</b-form-invalid-feedback
+      <b-form-invalid-feedback id="input-live-4"
+        >Enter a valid date</b-form-invalid-feedback
       >
-      <br />
-      <!-- genre -->
-      <div>
-        <label for="genre">Type a new genre and press enter</label>
-        <b-form-tags
-          input-id="genre"
-          placeholder="Add genre.."
-          v-model="game.genres"
-          :state="genresState"
-          aria-describedby="input-live-feedback5"
-        ></b-form-tags>
-        <b-form-invalid-feedback id="input-live-feedback5"
-          >Please select genre(s)</b-form-invalid-feedback
-        >
-        <br />
-        <!-- Image -->
-        <b-form-group id="urlInput" label="Image Url" label-for="urlInput">
-          <b-form-input
-            id="urlInput"
-            v-model="game.imageUrl"
-            :state="urlState"
-            aria-describedby="input-live-feedback2"
-            type="text"
-            placeholder="Image Url"
-            required
-          ></b-form-input>
-          <b-form-invalid-feedback id="input-live-feedback2"
-            >Enter at least 1 letter</b-form-invalid-feedback
-          >
-        </b-form-group>
+    </b-form-group>
 
-        <!-- platforms -->
-        <label for="platforms"
-          >Type a new platform and press enter
-          <span class="bracket"
-            >(NES, Xbox 360, Xbox one, PlayStation 3, PlayStation 4, PlayStation
-            5, PC, Nintendo 64, GameCube, Xbox X/S)</span
-          ></label
-        >
-        <br />
+    <!-- Image -->
+    <b-form-group id="urlInput" label="Image Url" label-for="urlInput">
+      <b-form-input
+        id="urlInput"
+        v-model="$v.game.imageUrl.$model"
+        :state="validateState('imageUrl')"
+        aria-describedby="input-live-feedback2"
+        type="text"
+        placeholder="Image Url"
+        required
+      ></b-form-input>
+      <b-form-invalid-feedback id="input-live-feedback2"
+        >Enter at least 1 letter</b-form-invalid-feedback
+      >
+    </b-form-group>
 
-        <b-form-tags
-          input-id="platforms"
-          placeholder="Add platform.."
-          v-model="game.platforms"
-          :state="platformsState"
-          aria-describedby="input-live-feedback6"
-        ></b-form-tags>
-        <b-form-invalid-feedback id="input-live-feedback6"
-          >Please select platform(s)</b-form-invalid-feedback
-        >
-        <br />
+    <!-- genre -->
+    <b-form-group>
+      <label for="genre">Select genre(s):</label>
+      <b-form-tags
+        required
+        input-id="genre"
+        placeholder="Pick some"
+        v-model="$v.game.genres.$model"
+        aria-describedby="input-live-5"
+        tag-placeholder="Add this as new tag"
+      ></b-form-tags>
+      <b-form-invalid-feedback id="input-live-5"
+        >Please enter valid genre(s)</b-form-invalid-feedback
+      >
+    </b-form-group>
 
-        <!-- publishers -->
-        <label for="Publishers">Type a new Publisher and press enter </label>
-        <b-form-tags
-          input-id="Publishers"
-          placeholder="Add Publishers.."
-          v-model="game.publishers"
-          :state="publisherState"
-          aria-describedby="input-live-feedback7"
-        ></b-form-tags>
-        <b-form-invalid-feedback id="input-live-feedback7"
-          >Please select publisher(s)</b-form-invalid-feedback
-        >
-        <br />
-
-        <!-- developers -->
-        <label for="developers">Type a new Developer and press enter </label>
-        <b-form-tags
-          input-id="developers"
-          placeholder="Add Developers.."
-          v-model="game.developers"
-          :state="developersState"
-          aria-describedby="input-live-feedback8"
-        >
-        </b-form-tags>
-        <b-form-invalid-feedback id="input-live-feedback8"
-          >Please select developer(s)</b-form-invalid-feedback
-        >
-        <br />
-      </div>
-      <input type="submit" v-on:click="submitEdit" />
-    </b-form>
-  </div>
+    <b-form-group>
+      <!-- platforms -->
+      <label for="platforms"
+        >Type a new platform and press enter
+        <span class="bracket"
+          >(NES, Xbox 360, Xbox one, PlayStation 3, PlayStation 4, PlayStation
+          5, PC, Nintendo 64, GameCube, Xbox X/S)</span
+        ></label
+      >
+      <b-form-tags
+        input-id="platforms"
+        placeholder="Add platform.."
+        v-model="$v.game.platforms.$model"
+        :state="validateState('platforms')"
+        required
+        aria-describedby="input-live-6"
+      ></b-form-tags>
+      <b-form-invalid-feedback id="input-live-6"
+        >Please enter valid platform(s)</b-form-invalid-feedback
+      >
+    </b-form-group>
+    <br />
+    <b-form-group>
+      <!-- publishers -->
+      <label for="Publishers">Type a new Publisher and press enter </label>
+      <b-form-tags
+        input-id="Publishers"
+        placeholder="Add Publishers.."
+        v-model="$v.game.publishers.$model"
+        :state="validateState('publishers')"
+        required
+        aria-describedby="input-live-7"
+      ></b-form-tags>
+      <b-form-invalid-feedback id="input-live-7"
+        >Please enter valid publisher(s)</b-form-invalid-feedback
+      >
+    </b-form-group>
+    <br />
+    <b-form-group>
+      <!-- developers -->
+      <label for="developers">Type a new Developer and press enter </label>
+      <b-form-tags
+        input-id="developers"
+        placeholder="Add Developers.."
+        v-model="$v.game.developers.$model"
+        :state="validateState('developers')"
+        required
+        aria-describedby="inp-liv-8"
+      ></b-form-tags>
+      <b-form-invalid-feedback id="inp-liv-8"
+        >Please enter valid developer(s)</b-form-invalid-feedback
+      >
+    </b-form-group>
+    <input type="submit" variant="primary" />
+  </b-form>
 </template>
 
 <script>
+import { required, minLength, alpha, url } from "vuelidate/lib/validators";
 import gameService from "../services/GameService.js";
 export default {
   data() {
@@ -166,48 +184,57 @@ export default {
       },
     };
   },
-  computed: {
-    titleState() {
-      return this.game.title.length > 0 ? true : false;
-    },
-    descriptionState() {
-      return this.game.description.length > 0 ? true : false;
-    },
-    esrbRatingState() {
-      return this.game.esrbRating != null ? true : false;
-    },
-    releaseDateState() {
-      return this.game.releaseDate != "" ? true : false;
-    },
-    urlState() {
-      return this.game.imageUrl != "" ? true : false;
-    },
-    genresState() {
-      return this.game.genres != "" ? true : false;
-    },
-    platformsState() {
-      return this.game.platforms != "" ? true : false;
-    },
-    publisherState() {
-      return this.game.publishers != "" ? true : false;
-    },
-    developersState() {
-      return this.game.developers != "" ? true : false;
+  validations: {
+    game: {
+      title: {
+        required,
+        minLength: minLength(1),
+      },
+      description: {
+        required,
+        minLength: minLength(1),
+      },
+      esrbRating: {
+        required,
+        alpha,
+      },
+      releaseDate: {
+        required,
+      },
+      genres: {
+        required,
+      },
+      platforms: {
+        required,
+      },
+      publishers: {
+        required,
+      },
+      developers: {
+        required,
+      },
+      imageUrl: {
+        required,
+        url,
+      },
     },
   },
   methods: {
+    onSubmit() {
+      this.$v.game.$touch();
+      if (this.$v.game.$anyError) {
+        return;
+      }
+      this.submitEdit();
+    },
+    validateState(state) {
+      const { $dirty, $error } = this.$v.game[state];
+      return $dirty ? !$error : null;
+    },
     submitEdit() {
       console.log("Reached");
       if (
-        this.titleState &&
-        this.descriptionState &&
-        this.esrbRatingState &&
-        this.releaseDateState &&
-        this.genresState &&
-        this.game.imageUrl &&
-        this.platformsState &&
-        this.publisherState &&
-        this.developersState
+        this.game
       ) {
         gameService.editGame(this.game).then(() => {
           gameService.list();
