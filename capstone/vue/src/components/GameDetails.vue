531b7eb52048ed6931a39c0ad44f1facd79553ea
Kevin Lie <kevinyilie@gmail.com>
2023-08-08 15:50:29 -0400

    Change GameDetails to have table format instead of list. Add GameDescription

--- capstone/vue/src/components/GameDetails.vue
+++ capstone/vue/src/components/GameDetails.vue
@@ -1,81 +1,43 @@
 <template>
-  <div class="container">
-    <h1 class="title">{{ item.title }}</h1>
-    <img v-bind:src="item.imageUrl" />
-
-    Description:
-    <p class="description">{{ item.description }}</p>
-
-    <ul class="platforms">
-      Platforms:
-      <li v-for="platform in item.platforms" v-bind:key="platform">
-        {{ platform }}
-      </li>
-    </ul>
-    <ul class="developers">
-      Developers:
-      <li v-for="developer in item.developers" v-bind:key="developer">
-        {{ developer }}
-      </li>
-    </ul>
-    <ul class="publishers">
-      Publishers:
-      <li v-for="publisher in item.publishers" v-bind:key="publisher">
-        {{ publisher }}
-      </li>
-    </ul>
-    <ul class="genres">
-      Genres:
-      <li v-for="genre in item.genres" v-bind:key="genre">
-        {{ genre }}
-      </li>
-    </ul>
+  <div class="container ">
+    <table class="table">
+      <tbody>
+        <tr>
+          <th scope="row">Release Date</th>
+          <td>{{ new Date(item.releaseDate).toLocaleString("en", options)  }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Genre</th>
+          <td>{{ item.genres.join(", ") }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Platform</th>
+          <td>{{ item.platforms.join(", ") }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Developer</th>
+          <td>{{ item.developers.join(", ") }}</td>
+        </tr>
+             <tr>
+          <th scope="row">Publisher</th>
+          <td>{{ item.publishers.join(", ") }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   props: ["item"],
+    data() {
+    return {
+      options: { year: "numeric", month: "long", day: "numeric" },
+    };
+  },
 };
 </script>
 
 <style scoped>
-img {
-  justify-self: center;
-}
-.container {
-  display: grid;
-  grid-template: 1fr 1fr;
-  grid-template-areas:
-    ". title "
-    "description"
-    "genre "
-    "platforms "
-    "developers "
-    "publishers ";
-}
-.title {
-  grid-area: "title";
-}
-.description {
-  grid-area: "description";
-}
-.genre {
-  grid-area: genre;
-}
-.platforms {
-  grid-area: "platforms";
-}
-.developers {
-  grid-area: "developers";
-}
-.publishers {
-  grid-area: "publishers";
-}
-P,
-h1,
-ul,
-li {
-  border: 1px solid black;
-}
+
 </style>
