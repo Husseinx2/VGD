ae684c397d7d7a68cc770ddc23c7ea4d929de42c
hussein hussein <husseinwkc@gmail.com>
2023-08-09 17:33:48 -0400

    controllers set up

--- capstone/dotnet/Capstone/Controllers/RatingController.cs
+++ capstone/dotnet/Capstone/Controllers/RatingController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Capstone.DAO.Interfaces;
+using Capstone.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Capstone.Controllers
 {
@@ -7,5 +9,97 @@ namespace Capstone.Controllers
     [ApiController]
     public class RatingController : ControllerBase
     {
+        public IRatingDao ratingDao;
+
+        public RatingController(IRatingDao ratingDao)
+        {
+            this.ratingDao = ratingDao;
+        }
+
+        [HttpGet("game/{gameId}")]
+        public ActionResult<List<Rating>> ListRatingsByGameId(int gameId)
+        {
+            List<Rating> ratings = ratingDao.ListRatingsByGameId(gameId);
+            if (ratings != null)
+            {
+                return Ok(ratings);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("user/{userId}")]
+        public ActionResult<List<Rating>> ListRatingsByUserId(int userId)
+        {
+            List<Rating> ratings = ratingDao.ListRatingsByUserId(userId);
+            if (ratings != null)
+            {
+                return Ok(ratings);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("/GetRating/{gameId}/{userId}")]
+        public ActionResult<Rating> GetRating(int gameId, int userId)
+        {
+            Rating rating = ratingDao.GetRating(gameId, userId);
+            if (rating != null)
+            {
+                return Ok(rating);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost()]
+        public ActionResult<Rating> AddRating(Rating rating)
+        {
+            rating.RatingId = 0;
+            Rating newRating = ratingDao.AddRating(rating);
+            if (newRating != null || newRating.RatingId == 0)
+            {
+                return Ok(newRating);
+            } 
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut()]
+        public ActionResult<Rating> UpdateRating(Rating rating)
+        {
+            Rating newRating = ratingDao.UpdateRating(rating);
+            if (newRating != null)
+            {
+                return Ok(newRating);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("/{gameId}")]
+        public ActionResult<bool> DeleteRating(int gameId)
+        {
+            bool result =ratingDao.DeleteRating(gameId);
+            if (result)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return StatusCode(400, result);
+            }
+        }
+
     }
 }
--- capstone/dotnet/Capstone/DAO/Interfaces/IRatingDao.cs
+++ capstone/dotnet/Capstone/DAO/Interfaces/IRatingDao.cs
@@ -6,12 +6,13 @@ namespace Capstone.DAO.Interfaces
     public interface IRatingDao
     {
         public List<Rating> ListRatingsByGameId(int gameId);
+        public List<Rating> ListRatingsByUserId(int userId);
 
         public Rating GetRating(int gameId, int userId);
 
-        public Rating AddRating(int gameId, int userId);
+        public Rating AddRating(Rating rating);
 
-        public Rating UpdateRating(int gameId, int userId);
+        public Rating UpdateRating(Rating rating);
 
         public bool DeleteRating(int gameId);
     }
