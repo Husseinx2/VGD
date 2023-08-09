4643abba4694d915a81414590eccb91ea7da1771
Adam Kasper <adam@ayriana.com>
2023-08-09 16:34:32 -0400

    pushing

--- capstone/dotnet/Capstone/Models/Rating.cs
+++ capstone/dotnet/Capstone/Models/Rating.cs
@@ -1,6 +1,14 @@
-﻿namespace Capstone.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone.Models
 {
-    public class Comments
+    public class Rating
     {
+        public int RatingId { get; set; }
+        public int UserId { get; set; }
+        public int GameId { get; set; }
+        public int Value { get; set; }
+        public DateTime DatePosted { get; set; }
     }
 }
