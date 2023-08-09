e99fbd89f58211a30283923c99e3268891c119c4
Kevin Lie <kevinyilie@gmail.com>
2023-08-09 10:10:26 -0400

    Move edit and delete button in GameDescription to top right corner

--- capstone/vue/src/components/GameDescription.vue
+++ capstone/vue/src/components/GameDescription.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="card container my-5">
+    <div class="container mt-2">
+      <span v-show="$store.state.user.role == 'admin'">
+        <div class="b-btn-toolbar float-right">
+          <b-button-group class="mx-1">
+            <b-button
+              class="btn btn-warning"
+              v-bind:to="{ name: 'edit', params: { id: item.id } }"
+            >
+              Edit</b-button
+            >
+          </b-button-group>
+          <b-button-group class="mx-1">
+            <b-button
+              class="btn btn-danger"
+              v-bind:to="{ name: 'delete', params: { id: item.id } }"
+              >Delete</b-button
+            >
+          </b-button-group>
+        </div>
+      </span>
+    </div>
     <div class="p-5 text-center bg-body-tertiary rounded-3">
-        <b-img :src="item.imageUrl" fluid alt=""></b-img>
-        <h1>{{ item.title }}</h1>
-        <p>{{ item.description }}</p>
-        <p>{{ new Date(item.releaseDate).toLocaleString("en", options) }}</p>
-        <span v-show="$store.state.user.role == 'admin'">
-          <div class="b-btn-toolbar">
-            <b-button-group class="mx-1">
-              <b-button
-                class="btn btn-warning"
-                v-bind:to="{ name: 'edit', params: { id: item.id } }"
-              >
-                Edit</b-button
-              >
-            </b-button-group>
-            <b-button-group class="mx-1">
-              <b-button
-                class="btn btn-danger"
-                v-bind:to="{ name: 'delete', params: { id: item.id } }"
-                >Delete</b-button
-              >
-            </b-button-group>
-          </div>
-        </span>
+      <b-img :src="item.imageUrl" fluid alt=""></b-img>
+      <h1>{{ item.title }}</h1>
+      <p>{{ item.description }}</p>
+      <p>{{ new Date(item.releaseDate).toLocaleString("en", options) }}</p>
     </div>
   </div>
 </template>
