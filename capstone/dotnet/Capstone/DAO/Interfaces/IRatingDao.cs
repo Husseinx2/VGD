be04ce458178c6337514b45b0d7a138b9d32348d
hussein hussein <husseinwkc@gmail.com>
2023-08-09 17:46:24 -0400

    dao and controller set

--- capstone/dotnet/Capstone/DAO/Interfaces/IRatingDao.cs
+++ capstone/dotnet/Capstone/DAO/Interfaces/IRatingDao.cs
@@ -7,8 +7,9 @@ namespace Capstone.DAO.Interfaces
     {
         public List<Rating> ListRatingsByGameId(int gameId);
         public List<Rating> ListRatingsByUserId(int userId);
+        public Rating GetRating(int gameId, int userId);
         public Rating AddRating(Rating rating);
         public Rating UpdateRating(Rating rating);
-        public bool DeleteRating(int ratingId);
+        public bool DeleteRating(int gameId,int userId);
     }
 }
