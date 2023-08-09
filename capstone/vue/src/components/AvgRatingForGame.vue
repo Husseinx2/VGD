08c3de6885d117b5bf3dd201c41a5d2b6e170f56
hussein hussein <husseinwkc@gmail.com>
2023-08-09 18:37:41 -0400

    ratings

--- capstone/vue/src/components/AvgRatingForGame.vue
+++ capstone/vue/src/components/AvgRatingForGame.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+  <div>
     <label for="rating">Average Rating</label>
     <b-form-rating size="sm" no-border v-model="value" readonly></b-form-rating>
   </div>
@@ -8,27 +8,41 @@
 <script>
 import ratingService from "../services/RatingService.js";
 export default {
- data() {
+  data() {
     return {
-        user: {
-            userRating: null,
-        }
-    }
- },
- methods: {//temporary till we get the server side working
-    getAvgRatingValue(){
-        if(this.user.userRating){
-            ratingService
-            .getRating(this.user.userRating)
-            .then(() => {
-            })
-            .catch()
-        }
-    }
- }
-}
+  props: ["game"],
+      avgRating: 0,
+      ratings: [],
+      user: {
+        userRating: null,
+      },
+    };
+  },
+  methods: {
+    //temporary till we get the server side working
+    getAvgRatingValue() {
+      if (this.user.userRating) {
+        ratingService
+          .getRating(this.user.userRating)
+          .then(() => {})
+          .catch();
+      }
+    },
+    GetRatings() {
+      ratingService.getRatingByGameId(this.gameId).then((response) => {
+        this.ratings = response.data;
+      });
+    },
+
+  },
+  computed: {
+
+  },
+  created() {
+    this.GetRatings();
+  },
+};
 </script>
 
 <style>
-
 </style>
--- capstone/vue/src/services/RatingService.js
+++ capstone/vue/src/services/RatingService.js
@@ -3,5 +3,8 @@ import axios from 'axios';
 export default {
   getRating(user) {
     return axios.get(`/rating/${user.userRating}`, user)
-    }
+    },
+  getRatingByGameId(id){
+    return axios.get(`/rating/game/${id}`);
+  }
 }
