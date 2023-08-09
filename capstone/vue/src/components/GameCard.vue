9b7a29d1bf08723bf92d68a083efd0c8d5dcf255
Kevin Lie <kevinyilie@gmail.com>
2023-08-09 10:29:35 -0400

    Show edit and delete buttons in GameCard admin only

--- capstone/vue/src/components/GameCard.vue
+++ capstone/vue/src/components/GameCard.vue
@@ -5,26 +5,28 @@
       v-bind:to="{ name: 'game', params: { id: item.id } }"
     >
       <b-card footer-tag="footer">
-        <template #header >
+        <template #header>
           <h2 class="mb-0">{{ item.title }}</h2>
         </template>
         <b-card-img v-bind:src="item.imageUrl" style="max-width: 20rem" />
         <b-card-text>{{ item.description }}</b-card-text>
-        <b-button-group class="mx-1">
-          <b-button
-            class="btn btn-warning"
-            v-bind:to="{ name: 'edit', params: { id: item.id } }"
-          >
-            Edit</b-button
-          >
-        </b-button-group>
-        <b-button-group class="mx-1">
-          <b-button
-            class="btn btn-danger"
-            v-bind:to="{ name: 'delete', params: { id: item.id } }"
-            >Delete</b-button
-          >
-        </b-button-group>
+        <div v-show="$store.state.user.role == 'admin'">
+          <b-button-group class="mx-1">
+            <b-button
+              class="btn btn-warning"
+              v-bind:to="{ name: 'edit', params: { id: item.id } }"
+            >
+              Edit</b-button
+            >
+          </b-button-group>
+          <b-button-group class="mx-1">
+            <b-button
+              class="btn btn-danger"
+              v-bind:to="{ name: 'delete', params: { id: item.id } }"
+              >Delete</b-button
+            >
+          </b-button-group>
+        </div>
         <template #footer>
           <h3>
             {{ new Date(item.releaseDate).toLocaleString("en", options) }}
@@ -47,18 +49,15 @@ export default {
 </script>
 
 <style scoped>
-
 div.card {
   border: 1px solid rgb(187, 187, 206);
   width: 40%;
   text-align: center;
 }
 
-
 div.card:hover {
   border: 1px solid black;
   background-color: rgb(195, 202, 202);
   cursor: pointer;
 }
-
 </style>
