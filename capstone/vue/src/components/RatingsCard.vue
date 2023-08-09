3db1adce4e0ca9742d5df7729c0f0e9ae9d22e78
Jimmie Nakadaira <moldychip@hotmail.com>
2023-08-09 18:05:38 -0400

    creating star rating components

---
+++ capstone/vue/src/components/RatingsCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    <label for="rating-inline-center">How would you rate this game?</label>
+    <b-form-rating size="lg" no-border show-clear class="rating" id="rating-inline-center" inline value="0" color="orange"></b-form-rating>
+  </div>
+</template>
+
+<script>
+import ratingService from "../services/RatingService.js";
+export default {
+ data() {
+    return {
+        user: {
+            userRating: null,
+        }
+    }
+ },
+ methods: {
+    createRatingValue(){
+        if(this.user.userRating){
+            ratingService
+            .getRating(this.user.userRating)
+            .then(() => {
+            })
+            .catch()
+        }
+    }
+ }
+}
+</script>
+
+<style scoped>
+.rating{
+    align-content: center;
+    vertical-align: middle;
+    
+    
+}
+</style>
