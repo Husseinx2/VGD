d898213445ea457cc05988e4223270c06c9f3935
Adam Kasper <adam@ayriana.com>
2023-08-10 14:01:49 -0400

    pushing files

--- capstone/vue/src/views/Profile.vue
+++ capstone/vue/src/views/Profile.vue
@@ -4,8 +4,8 @@
       <h1>
           Profile
       </h1>
-      <profile-comp />
-      <profile-rating />
+      <profile-comp v-bind:item="$store.state.user.userId" />
+      <profile-rating  v-bind:item="$store.state.user.userId"/>
       </main>
   </div>
 </template>
@@ -13,22 +13,10 @@
 <script>
 import ProfileComp from '../components/ProfileComp.vue';
 import ProfileRating from '../components/ProfileRating.vue';
-import RatingService from "../services/RatingService";
 
 export default {
 components: {ProfileComp, ProfileRating},
-
- methods: {
-    push() {
-      this.$router.push("/");
-    },
-    ListRatingsByUserId() {
-      RatingService.ListRatingsByUserId(this.game.id).then(() => {
-        this.$router.push("/");
-      });
-    },
-  },
-} 
+}
 </script>
 
 <style>
