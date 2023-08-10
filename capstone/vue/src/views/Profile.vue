08ebcd548522f49958ab02b69e3d8a0a3b3a234d
Adam Kasper <adam@ayriana.com>
2023-08-10 17:10:58 -0400

    pushing changes

--- capstone/vue/src/views/Profile.vue
+++ capstone/vue/src/views/Profile.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Profile" style="font-family: Georgia, 'Times New Roman', Times, serif;">
       <main>
-      <h1 style="margin-left:20px; margin-top:12px; margin-bottom:10px; font-family: Georgia, 'Times New Roman', Times, serif; color:#3e75e2;">
+      <h1 style="margin-left:20px; margin-top:12px; margin-bottom:10px; font-family: Georgia, 'Times New Roman', Times, serif; color:#3e75e2; fontWeight:600;">
           Profile
       </h1>
       <profile-comp style="margin-left:60px; margin-top:12px; margin-bottom:10px; font-family: Georgia, 'Times New Roman', Times, serif;" v-bind:item="$route.params.id" />
