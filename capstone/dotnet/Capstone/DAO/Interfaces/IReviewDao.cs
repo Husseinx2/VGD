b2870cfcb1dd35215c1146f69e8338d0886ac807
Kevin Lie <kevinyilie@gmail.com>
2023-08-12 13:12:00 -0400

    Modify Review model and IReviewDao

--- capstone/dotnet/Capstone/DAO/Interfaces/IReviewDao.cs
+++ capstone/dotnet/Capstone/DAO/Interfaces/IReviewDao.cs
@@ -8,13 +8,18 @@ namespace Capstone.DAO.Interfaces
     {
         public List<Review> ListReviewsByGameId(int gameId);
 
+        public List<Review> ListReviewsByReviewerId(int reviewerId);
+
         public Review GetReview(int reviewId);
 
         public Review AddReview(Review review);
 
         public Review UpdateReview(Review review);
 
-        public List<Review> ListReviewsByReviewId(int reviewId);
         public bool DeleteReview(int reviewId);
+
+        public bool DeleteReviewsByGameId(int gameId);
+
+        public bool DeleteReviewsByReviewerId(int reviewerId);
     }
 }
