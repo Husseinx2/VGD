147d539aab1e9b7c9c053142fc2792752691236a
Kevin Lie <kevinyilie@gmail.com>
2023-08-12 16:28:39 -0400

    Add logo to footer

--- capstone/vue/src/components/TheFooter.vue
+++ capstone/vue/src/components/TheFooter.vue
@@ -2,13 +2,14 @@
   <div class="container">
 
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p class="col-md-4 mb-0 text-body-secondary">© 2023 VGD, Inc</p>
+    <p class="col-md-3 mb-0 text-body-secondary">© 2023 VGD, Inc</p>
 
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+    <a href="/" class="col-md-3 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <img class="navbar-logo bi me-2" width="60" height="32" src="./..\Images\logo1.png" />
     </a>
 
-    <ul class="nav col-md-4 justify-content-end">
+
+    <ul class="nav col-md-3 justify-content-end">
       <li class="nav-item"><a href="/Features" class="nav-link px-2 text-body-secondary">Features</a></li>
       <li class="nav-item"><a href="/FAQ" class="nav-link px-2 text-body-secondary">FAQs</a></li>
       <li class="nav-item"><a href="/About" class="nav-link px-2 text-body-secondary">About</a></li>
