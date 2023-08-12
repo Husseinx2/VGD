0e276549410a1b6c915f85819eb11522f20cd695
hussein hussein <husseinwkc@gmail.com>
2023-08-12 13:59:25 -0400

    review service set up

--- capstone/vue/src/services/ReviewService.js
+++ capstone/vue/src/services/ReviewService.js
@@ -1,13 +1,29 @@
 import axios from 'axios';
 
 export default {
-    addReview() {
-        return axios.post(``);
+    addReview(review) {
+        return axios.post(`/review`, review);
     },
-    getUserReviews(){
-        return axios.get(``);
+    getUserReviews(id) {
+        return axios.get(`review/reviewer/${id}`);
     },
-    getGameReviews(){
-        return axios.get(``);
+    getGameReviews(id) {
+        return axios.get(`review/game/${id}`);
+    },
+    getReview(id) {
+        return axios.get(`review/${id}`);
+    },
+    editReview(review) {
+        axios.put('review', review);
+    },
+    deleteReview(id){
+        axios.put( `review/${id}`);
+    },
+    deleteReviewByGameId(id){
+        axios.put( `review/game/${id}`);
+    },
+    deleteReviewByReviewerId(id){
+        axios.put( `review/reviewer/${id}`);
     }
+
 };
