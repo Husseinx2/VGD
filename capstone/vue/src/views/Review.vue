4ee0e7528fcaa4cf8e94dcf665806db183cbdd8d
Kevin Lie <kevinyilie@gmail.com>
2023-08-13 15:03:28 -0400

    Add Review and ReviewCard

--- capstone/vue/src/views/Review.vue
+++ capstone/vue/src/views/Review.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>
+  <div class="container">
+    <review-card
+    
+      v-bind:item="review"
+      class="list-reviews-card"
+    />
   </div>
 </template>
 
+
 <script>
+import ReviewCard from '../components/ReviewCard.vue';
+import reviewService from "../services/ReviewService.js";
 export default {
+  components: { ReviewCard },
+  data() {
+  
+  return {
+      id: 0,
 
-
-}
+      review: null,
+    };
+  },
+  created() {
+    this.id = Number.parseInt(this.$route.params.id);
+    reviewService.getReview(this.id)
+    .then((response) => {this.review = response.data})
+  },
+};
 </script>
 
 <style>
-
 </style>
