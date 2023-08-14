46913cff5d62ff0c7f6a7483d8011606c8b6e021
Kevin Lie <kevinyilie@gmail.com>
2023-08-14 16:18:44 -0400

    Add List Model

---
+++ capstone/dotnet/Capstone/Models/List.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Capstone.Models
+{
+    public class List
+    {
+        public int ListId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string ListTitle { get; set; }
+
+        public string ListType { get; set; }
+
+        public List<Game> Games { get; set; }
+    }
+}
