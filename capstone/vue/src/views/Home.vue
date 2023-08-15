5e7b3b09f862695f7096bd6bce6987d073c3b643
Kevin Lie <kevinyilie@gmail.com>
2023-08-15 11:15:43 -0400

    Add jumbotron

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home container">
-
     <b-alert :show="gameAdded" variant="success" role="alert" dismissible>
       Game Successfully Added
     </b-alert>
@@ -13,24 +12,30 @@
       Game Successfully Deleted
     </b-alert>
     <carousel />
+    <br />
+    <jumbotron />
+    <br />
     <what-to-play />
     <back-to-top />
-
   </div>
 </template>
 
 <script>
 import BackToTop from "../components/BackToTop.vue";
-import Carousel from '../components/Carousel.vue';
-import WhatToPlay from '../components/WhatToPlay.vue';
+import Carousel from "../components/Carousel.vue";
+import Jumbotron from "../components/Jumbotron.vue";
+import WhatToPlay from "../components/WhatToPlay.vue";
 
 export default {
   name: "home",
-   components: { BackToTop, WhatToPlay, Carousel },
+  components: { BackToTop, WhatToPlay, Carousel, Jumbotron },
   computed: {
     filteredList() {
       return this.games.filter((game) => {
-        return game.title.toLowerCase().includes(this.search) || game.description.toLowerCase().includes(this.search) ;
+        return (
+          game.title.toLowerCase().includes(this.search) ||
+          game.description.toLowerCase().includes(this.search)
+        );
       });
     },
   },
