5a106410f5cee7c2400542fff165c70d0fd14efd
Kevin Lie <kevinyilie@gmail.com>
2023-08-16 10:24:55 -0400

    Add profile router-link to CommentCard

--- capstone/vue/src/components/CommentCard.vue
+++ capstone/vue/src/components/CommentCard.vue
@@ -1,38 +1,51 @@
 <template>
-  <section >
-    <b-card  class="comments-card">
+  <section>
+    <b-card class="comments-card">
       <b-card-header class="font-weight-bold">
-        <table>
-          <td>
-        <b-avatar  :variant="commenter.username == isAdmin ? 'danger' : 'primary'">
-          </b-avatar> 
-          </td>
-          <td>
-          <p class="username" :class="isItalics" :style="changeFont" v-text="isDeleted" ></p>
-          </td>
-        </table>
-          </b-card-header>
+        <router-link
+          class="profile-link"
+          v-bind:to="{ name: 'profile', params: { id: item.commenterId } }"
+        >
+          <table>
+            <td>
+              <b-avatar
+                :variant="commenter.username == isAdmin ? 'danger' : 'primary'"
+              >
+              </b-avatar>
+            </td>
+            <td>
+              <p
+                class="username"
+                :class="isItalics"
+                :style="changeFont"
+                v-text="isDeleted"
+              ></p>
+            </td>
+          </table>
+        </router-link>
+      </b-card-header>
       <!--game title -->
       <b-card-body class="body">
-        <b-card-text
-        v-show="!showEdit"
-        > {{ item.commentContent }} 
+        <b-card-text v-show="!showEdit">
+          {{ item.commentContent }}
         </b-card-text>
         <b-form @submit.stop.prevent="onSubmit">
-        <b-form-textarea
-        type="text"
-        aria-describedby="input-live-feedback"
-        v-model="$v.comment.commentContent.$model"
-        :state="validateState('commentContent')"
-        v-show="showEdit"
-        required
-      > 
-        </b-form-textarea>
-        <b-form-invalid-feedback id="input-live-feedback"
-        >Enter at least 1 letter</b-form-invalid-feedback
-      >    
-      <b-button v-show="showEdit" type="submit" variant="primary">Submit</b-button>
-      <br/> 
+          <b-form-textarea
+            type="text"
+            aria-describedby="input-live-feedback"
+            v-model="$v.comment.commentContent.$model"
+            :state="validateState('commentContent')"
+            v-show="showEdit"
+            required
+          >
+          </b-form-textarea>
+          <b-form-invalid-feedback id="input-live-feedback"
+            >Enter at least 1 letter</b-form-invalid-feedback
+          >
+          <b-button v-show="showEdit" type="submit" variant="primary"
+            >Submit</b-button
+          >
+          <br />
         </b-form>
         <br />
         <b-card-footer class="footer">
@@ -40,24 +53,30 @@
           {{ new Date(item.commentDateTime).toLocaleString("en", options) }}
         </b-card-footer>
         <b-button-group>
-        <b-button
-          class="btn btn-warning"
-          v-bind:key="item.commentId"
-          @click="showEdit = !showEdit"
-          v-show="$store.state.user.role == 'admin' || $store.state.user.userId == item.commenterId"
-        >
-          Edit <b-icon icon="pencil-fill" />
-        </b-button>
+          <b-button
+            class="btn btn-warning"
+            v-bind:key="item.commentId"
+            @click="showEdit = !showEdit"
+            v-show="
+              $store.state.user.role == 'admin' ||
+              $store.state.user.userId == item.commenterId
+            "
+          >
+            Edit <b-icon icon="pencil-fill" />
+          </b-button>
         </b-button-group>
         <b-button-group>
-        <b-button
-          class="btn btn-danger"
-          v-b-modal="`${item.commentContent}`"
-          v-bind:key="item.commentId"
-          v-show="$store.state.user.role == 'admin' || $store.state.user.userId == item.commenterId"
-        >
-          Delete <b-icon icon="trash" />
-        </b-button>
+          <b-button
+            class="btn btn-danger"
+            v-b-modal="`${item.commentContent}`"
+            v-bind:key="item.commentId"
+            v-show="
+              $store.state.user.role == 'admin' ||
+              $store.state.user.userId == item.commenterId
+            "
+          >
+            Delete <b-icon icon="trash" />
+          </b-button>
         </b-button-group>
 
         <b-modal
@@ -82,8 +101,8 @@ export default {
   props: ["item"],
   data() {
     return {
-      isAdmin: 'admin',
-      currentGame:{},
+      isAdmin: "admin",
+      currentGame: {},
       showEdit: false,
       comment: {
         commentId: this.item.commentId,
@@ -96,38 +115,34 @@ export default {
       commenter: null,
     };
   },
-  computed:{
-    changeFont(){
-        if(this.commenter.isDeleted){
-         return "font-size:13px"
-      }
-      else{
-        return ""
-      }
-    },
-    isItalics(){
-      if(this.commenter.isDeleted){
-        return "font-weight-normal font-italic font-size:13px"
-      }
-      else{
-        return ""
+  computed: {
+    changeFont() {
+      if (this.commenter.isDeleted) {
+        return "font-size:13px";
+      } else {
+        return "";
       }
     },
-    isDeleted(){
-      if(this.commenter.isDeleted){
-        return "Profile has been deactivated"
+    isItalics() {
+      if (this.commenter.isDeleted) {
+        return "font-weight-normal font-italic font-size:13px";
+      } else {
+        return "";
       }
-      else{
-        return this.commenter.username
+    },
+    isDeleted() {
+      if (this.commenter.isDeleted) {
+        return "Profile has been deactivated";
+      } else {
+        return this.commenter.username;
       }
-    }
+    },
   },
   mounted() {
-    console.log("reach mounted in Comment Card",this.item.commenterId)
-    userService
-      .GetUser(this.item.commenterId)
-      .then((response) => {
-        this.commenter = response.data})
+    console.log("reach mounted in Comment Card", this.item.commenterId);
+    userService.GetUser(this.item.commenterId).then((response) => {
+      this.commenter = response.data;
+    });
   },
   validations: {
     comment: {
@@ -138,11 +153,10 @@ export default {
     },
   },
   methods: {
-    Users(){
-      userService.listUsers()
-      .then((response) => {
-        this.users = response.data
-      })
+    Users() {
+      userService.listUsers().then((response) => {
+        this.users = response.data;
+      });
     },
     deleteComment() {
       console.log("reached");
@@ -175,12 +189,15 @@ export default {
       const { $dirty, $error } = this.$v.comment[state];
       return $dirty ? !$error : null;
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-.footer{
+.footer {
   font-size: 13px;
+}
+.profile-link {
+  color: black;
 }
 </style>
